cd42f5c0ce843ef76a0e2622837d247ac42cbb1a
Dan <danreedx86@gmail.com>
2015-03-28 17:59:26 -0700

    rebase larry in

--- game_needs_name.js
+++ game_needs_name.js
@@ -1,10 +1,16 @@
-
+var superDuper = this;
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var listener = new THREE.AudioListener();
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+
+var domEvents  = new THREEx.DomEvents(camera, renderer.domElement);
+
 document.body.appendChild( renderer.domElement );
+
+
 
 
 Leap.loop(function(frame) {
@@ -14,6 +20,24 @@ Leap.loop(function(frame) {
 });
 
 var cubes = [];
+
+var pewSound = new THREE.Audio( listener );
+pewSound.load( 'sound/pew.mp3' );
+
+var boomSound = new THREE.Audio( superDuper.listener );
+boomSound.load( 'sound/boom.mp3' );
+
+var goPew = function(mesh) {
+  superDuper.pewSound.play()
+}
+
+var goBoom = function(mesh) {
+  //LAAAAAAAAAAAAME -- Dan
+  if (superDuper.boomSound.isPlaying) {
+    superDuper.boomSound.stop()
+  }
+  superDuper.boomSound.play()
+}
 
 function newCurve() {
   return new THREE.QuadraticBezierCurve3(
@@ -32,8 +56,18 @@ setInterval(function(){
 
     var material = new THREE.MeshBasicMaterial( { color: color } );
     var cube = new THREE.Mesh( geometry, material );
+
     cube.x = Math.random();
     cube.y = Math.random();
+
+    var self = this
+    domEvents.addEventListener(
+      cube,
+      'click',
+      function() {
+        self.goBoom(cube)
+      },
+      false)
 
     cube.curve = newCurve();
     cube.curveLocation = 0;
