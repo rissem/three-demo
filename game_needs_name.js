10557dd793d8cad0443987b83e7bed78f7294e88
lkang <lkang@sbcglobal.net>
2015-03-28 23:42:15 -0700

    explosion creates 4 fastcubes. fixed object cleanup

--- game_needs_name.js
+++ game_needs_name.js
@@ -1,9 +1,11 @@
 var superDuper = this;
 var scene = new THREE.Scene();
 scene.fog = new THREE.Fog(0, -10, 80)
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-var listener = new THREE.AudioListener();
 
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 5;
+
+var listener = new THREE.AudioListener();
 var renderer = new THREE.WebGLRenderer();
 
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -18,7 +20,6 @@ Leap.loop(function(frame) {
 });
 
 var cubes = [];
-
 
 var pewSound = new THREE.Audio( listener );
 pewSound.load( 'sound/pew.mp3' );
@@ -38,14 +39,6 @@ var goBoom = function(mesh) {
   superDuper.boomSound.play()
 }
 
-function newCurve(start, end) {
-  return new THREE.QuadraticBezierCurve3(
-    start,
-    new THREE.Vector3( (Math.random() * 20.0) - 10, Math.random() * 20, -20 ),
-    end
-  );
-}
-
 setInterval(function(){
   var cube = Math.random() > 0.5 ? slowCube() : fastCube();
 
@@ -54,38 +47,76 @@ setInterval(function(){
   console.log(cubes.length);
 }, 1000)
 
-
-function Cube() {
+function Cube(startPoint, endPoint) {
   var self = this;
 
   var geometry = new THREE.BoxGeometry( 1.2, 1.2, 1.2 );
-  var color = '#' + (function co(lor){   return (lor +=
-[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)])
-&& (lor.length == 6) ?  lor : co(lor); })('');
+  var color = '#' + (function co(lor) {
+    return (
+      lor += [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.random()*16)]) && 
+      (lor.length == 6)  ?  lor : co(lor);
+    })('');
 
   var material = new THREE.MeshBasicMaterial( { color: color } );
-
   this.mesh = new THREE.Mesh( geometry, material );
 
-  this.x = Math.random();
-  this.y = Math.random();
-
-  var startPoint = new THREE.Vector3( (Math.random() * 10) - 5, -3, 0 ); //change this start point to the tip of the gun
-  var endPoint = new THREE.Vector3( 4.0, -3, -100 ); // change this to the target position
+  startPoint = startPoint || new THREE.Vector3( (Math.random() * 10) - 5, -3, 0 ); //change this start point to the tip of the gun
+  endPoint = endPoint || new THREE.Vector3( 4.0, -3, -100 ); // change this to the target position
 
   this.curve = newCurve(startPoint, endPoint);
+  this.curveLocation = 0;
+
+  function newCurve(start, end) {
+    return new THREE.QuadraticBezierCurve3(
+      start,
+      new THREE.Vector3( (Math.random() * 20.0) - 10, Math.random() * 20, -20 ),
+      end
+    );
+  }
 
   domEvents.addEventListener(
-    this.mesh,
+    this.mesh, 
     'click',
-    function() {
-      goBoom(self.mesh)
-      loc = cubes.indexOf(self)
-      self.mesh.material.color = 0;
-      self.velocity = 0.1; //make it go fast and disappear from the scene
-    }, false);
+    clickBoom,
+    false);
 
-  this.curveLocation = 0;
+  function clickBoom() {
+    goBoom(self.mesh);
+    self.explode();
+  }
+
+  this.remove = function() {
+    domEvents.removeEventListener(
+      self.mesh,
+      'click',
+      clickBoom,
+      false
+    );
+
+    scene.remove(this.mesh);
+    this.mesh.material.dispose();
+    this.mesh.material = null;
+    this.mesh.geometry.dispose();
+    this.mesh.geometry = null;
+    this.mesh = null;
+  }
+
+  this.explode = function() {
+    var c, startPoint;
+
+    this.remove();
+
+    var index = cubes.indexOf(this);
+    if(index > -1) cubes.splice(index, 1);
+
+    startPoint = this.curve.getPoint(this.curveLocation);
+
+    for(var i=0; i<4; i++) {
+      c = fastCube(startPoint)
+      cubes.push(c);
+      scene.add(c.mesh);
+    }
+  }
 }
 
 function slowCube() {
@@ -94,16 +125,14 @@ function slowCube() {
   return cube;
 }
 
-function fastCube() {
-  var cube = new Cube();
+function fastCube(startPoint, endPoint) {
+  var cube = new Cube(startPoint, endPoint);
   cube.velocity = 0.01;
   return cube;
 }
 
-camera.position.z = 5;
-
-var point;
-var render = function () {
+function render() {
+  var point;
   requestAnimationFrame( render );
 
   for (var i=0; i < cubes.length; i++){
@@ -118,6 +147,11 @@ var render = function () {
       cube.mesh.position.x = point.x
       cube.mesh.position.y = point.y
       cube.mesh.position.z = point.z
+
+      if(point.z < -50) {
+        cubes.splice(i, 1);
+        cube.remove();
+      }
   }
   renderer.render(scene, camera);
 };
